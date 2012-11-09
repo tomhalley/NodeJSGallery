9bb3ea2035cbeb341104f1f10a3bc7c20f925124
Tom Halley <tomhalley89@gmail.com>
2012-11-09 01:40:13 +0000

    refactored connect method into seperate module

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require("http");
 
-function onRequest(request, response) {
-	console.log("Request Received");
-	response.writeHeader(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+function start() {
+	function onRequest(request, response) {
+		console.log("Request Received");
+		response.writeHeader(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8080);
+
+	console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8080);
-
-console.log("Server has started.");
+exports.start = start;
